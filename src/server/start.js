ffef234ffc6c8a0daae639dcb030ee9af5542651
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-11-04 23:11:34 +0800

    fixed start script.

--- src/server/start.js
+++ src/server/start.js
@@ -4,12 +4,15 @@ import promptSync from 'prompt-sync'
 env.config()
 const prompt = promptSync()
 
-console.log('===== MYSQL CREDENTIALS =====')
+console.log('======== MYSQL CREDENTIALS ========')
 console.log('Press ENTER to leave default value')
+
 const host     = prompt('  Host [default=127.0.0.1]: ')
 const user     = prompt('       User [default=root]: ')
 const password = prompt('            Enter password: ', {echo: '*'})
 
+process.env.MYSQL_HOST = host
+process.env.MYSQL_USER = user
 process.env.MYSQL_PASSWORD = password
 
 import('./server.js')
