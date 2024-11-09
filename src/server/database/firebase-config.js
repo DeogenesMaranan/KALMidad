2188e4c008891ccdbfd11e5bba7d668d5074f34b
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-11-09 10:03:06 +0800

    connected project to firebase. Created user signin, signup, and signout methods.

---
+++ src/server/database/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEewTd9Kx9Aek-SXu0sWwpORQ3z7yFiyU",
+  authDomain: "wst-mc-project-server.firebaseapp.com",
+  projectId: "wst-mc-project-server",
+  storageBucket: "wst-mc-project-server.firebasestorage.app",
+  messagingSenderId: "180478976171",
+  appId: "1:180478976171:web:fa2d61ddbe1754bf4dd7ee"
+};
+
+export default initializeApp(firebaseConfig);
---
+++ src/server/database/user.js
@@ -0,0 +1,70 @@
+import { 
+    signOut,
+    getAuth,
+    setPersistence, 
+    browserSessionPersistence,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword, 
+} from 'firebase/auth'
+
+import firebaseApp from './firebase-config.js'
+import StringValidator from '../validation/string-validator.js'
+
+
+class User {
+    
+    #auth
+
+    constructor() {
+        this.#auth = getAuth(firebaseApp)
+    }
+
+    signin(email, password) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await setPersistence(this.#auth, browserSessionPersistence)
+
+                let userCredential = signInWithEmailAndPassword(this.#auth, email, password)
+                resolve((await userCredential).user)
+            } catch(error) {
+                reject(error)
+            }
+        })
+    }
+
+    signup(email, password) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await setPersistence(this.#auth, browserSessionPersistence)
+                this.#constraintPassword(password)
+                
+                let userCredential = createUserWithEmailAndPassword(this.#auth, email, password)
+                resolve((await userCredential).user)
+            } catch (error) {
+                reject(error)
+            }
+        });
+    }
+
+    signout() {
+        return new Promise (async (resolve, reject) => {
+            try {
+                await signOut(this.#auth)
+                resolve('Signout successful.')
+            } catch (error) {
+                reject('Error signing out.')
+            }
+        })
+    }
+    
+    #constraintPassword(p_password) {
+        if (!StringValidator.containsLowercaseLetter(p_password)){
+            throw new Error('Password must contain lower-cased characters.')
+        } 
+        if (StringValidator.containsNonNumericCharacter(p_password)) {
+            throw new Error('Password must only contain alphnumeric characters.')
+        }
+    }
+}
+
+export default new User()
