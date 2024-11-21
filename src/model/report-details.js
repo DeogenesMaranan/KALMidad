ec6f99975f7a88d5aba801e971e73c34330995e7
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-11-22 01:41:11 +0800

    displayed report history card in home page. IN PROGRESS

--- src/model/report-details.js
+++ src/model/report-details.js
@@ -5,23 +5,21 @@ class ReportDetails {
     #time
     #town
     #flag
+    #status
     #calamity
-    #lastname
-    #firstname
     #imageLink
     #description
 
-    constructor(calamity, city, date, time, description, flag,
-                lastname, imageLink, town, firstname
+    constructor(calamity, city, date, time, description, 
+                flag, imageLink, town, status
     ) {
         this.#city = city
         this.#time = time
         this.#date = date
         this.#town = town
         this.#flag = flag
+        this.#status = status
         this.#calamity = calamity
-        this.#lastname = lastname
-        this.#firstname = firstname
         this.#imageLink = imageLink
         this.#description = description
     }
@@ -31,9 +29,8 @@ class ReportDetails {
     get time() { return this.#time }
     get town() { return this.#town }
     get flag() { return this.#flag }
+    get status() { return this.#status }
     get calamity() { return this.#calamity }
-    get lastname() { return this.#lastname }
-    get firstname() { return this.#firstname }
     get imageLink() { return this.#imageLink }
     get description() { return this.#description }
     
@@ -42,9 +39,8 @@ class ReportDetails {
     set time(time) { this.#time = time }
     set town(town) { this.#town = town }
     set flag(flag) { this.#flag = flag }
+    set status(status) { this.#status = status }
     set calamity(calamity) { this.#calamity = calamity }
-    set lastname(lastname) { this.#lastname = lastname }
-    set firstname(firstname) { this.#firstname = firstname }
     set imageLink(imageLink) { this.#imageLink = imageLink }
     set description(description) { this.#description = description }
 }
