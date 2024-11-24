8aaf76751d8252c9832c2de77c85911e9baf398a
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-11-24 23:48:20 +0800

    displayed recent assessment. IN PROGRESS

--- src/web/presenter/recent-assessment.js
+++ src/web/presenter/recent-assessment.js
@@ -23,7 +23,8 @@ document.addEventListener('DOMContentLoaded', async () => {
         else if (userType === 'admin') {
             const response = await getAllReportsSubcollection()
             const userData = response.data.data
-            console.log('response', userData)
+            
+            displayTabularReportsAdmin(userData)
         }
     }
     catch(error) { 
@@ -58,11 +59,55 @@ function displayTabularReports(reportList) {
         image.src = report.imageLink
 
         imageRow.appendChild(image)
-        flagRow.textContent = (report.flag).toUpperCase()
-        calamityRow.textContent = (report.calamity).toUpperCase()
-        dateRow.textContent = UsToLongDateConverter(report.date)
-        locationRow.textContent = `${report.town}, ${report.city}`
-        statusRow.textContent = capitalize(report.status) 
+        flagRow.textContent        = (report.flag).toUpperCase()
+        calamityRow.textContent    = (report.calamity).toUpperCase()
+        dateRow.textContent        = UsToLongDateConverter(report.date)
+        locationRow.textContent    = `${report.town}, ${report.city}`
+        statusRow.textContent      = capitalize(report.status) 
         descriptionRow.textContent = capitalize(report.description)
+    })
+}
+
+function displayTabularReportsAdmin(reportList) {
+    document.getElementById('name-row').textContent = 'Report From'
+    const reportTable = document.getElementById('recent-assessment-table')
+                                .getElementsByTagName('tbody')[0]
+
+    reportList.forEach((report) => {
+        const newRow = reportTable.insertRow()
+
+        const imageRow       = newRow.insertCell(0)
+        const flagRow        = newRow.insertCell(1)
+        const calamityRow    = newRow.insertCell(2)
+        const nameRow        = newRow.insertCell(3)
+        const locationRow    = newRow.insertCell(4)
+        const statusRow      = newRow.insertCell(5)
+        const descriptionRow = newRow.insertCell(6)
+
+        const image = document.createElement('img')
+        image.src = report.imageLink
+
+        // const statusOption = ['Pending', 'In Process', 'Resolved']
+        // const statusSelect = document.createElement('select')
+        // statusSelect.id = 'status-select'
+
+        // statusOption.forEach((option) => {
+        //     const selectOpt = document.createElement('option')
+        //     selectOpt.textContent = option
+        //     selectOpt.value = option
+
+        //     statusSelect.appendChild(selectOpt)
+        // })
+
+        imageRow.appendChild(image)
+        // statusRow.appendChild(statusSelect)
+        
+        flagRow.textContent = report.flag
+        statusRow.textContent = capitalize(report.status)
+        // statusSelect.value = capitalize(report.status)
+        calamityRow.textContent = capitalize(report.calamity)
+        locationRow.textContent = `${report.town}, ${report.city}`
+        descriptionRow.textContent = capitalize(report.description)
+        nameRow.textContent = `${report.firstname} ${report.middlename[0]}. ${report.lastname}`
     })
 }
