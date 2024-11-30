3c5ed3058672eca8bf9ad2d358b13d0170b4cca1
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2024-12-01 00:22:57 +0800

    displayed top 5 cityies with most reports. FINISHED

--- src/web/presenter/home-admin.js
+++ src/web/presenter/home-admin.js
@@ -1,12 +1,12 @@
 
-import { getCount } from '../../services/request.js'
+import { getCount, getAllReportsSubcollection } from '../../services/request.js'
 
 const mildCase = document.getElementById('minor-case')
 const majorCase = document.getElementById('severe-case')
 const moderateCase = document.getElementById('moderate-case')
 const catastrophicCase = document.getElementById('catastrophic-case')
+const topCitiesListContainer = document.querySelector('.top-cities-list-container')
 const reportTotalNumberContainer = document.getElementById('report-total-number-container')
-
 
 document.addEventListener('DOMContentLoaded', async() => {
     try {
@@ -21,6 +21,45 @@ document.addEventListener('DOMContentLoaded', async() => {
         moderateCase.textContent = moderateCount.data.data
         catastrophicCase.textContent           = catastrophicCount.data.data  
         reportTotalNumberContainer.textContent = totalCount.data.data
+
+        const response = await getAllReportsSubcollection()
+        const citiesWithMostReports = getTopCities(response.data.data)
+        console.log(citiesWithMostReports)
+        displayTopCities(citiesWithMostReports)
     }
     catch (error) {console.error(error.message) }
 })
+
+function displayTopCities(dataList, number) {
+    number = number < 5? number : 5
+    
+    for(let idx = 0; idx < number; idx++) {
+        const cityInfoContainer = document.createElement('div')
+        const cityLabel = document.createElement('p')
+        const countLabel = document.createElement('p')
+
+        cityLabel.textContent = dataList[idx].item
+        countLabel.textContent = dataList[idx].frequency
+
+        cityInfoContainer.className = idx%2 === 1
+                                    ? 'city-info-container first'
+                                    : 'city-info-container second'
+        cityInfoContainer.appendChild(cityLabel)
+        cityInfoContainer.appendChild(countLabel)
+
+        topCitiesListContainer.appendChild(cityInfoContainer)
+    }
+}
+
+function getTopCities(dataList) {
+    const frequencyMap = {};
+
+    dataList.forEach((data) => {
+        frequencyMap[data.city] = (frequencyMap[data.city] || 0) + 1
+    })
+
+    const frequencyArray = Object.entries(frequencyMap)
+    frequencyArray.sort((a, b) => b[1] - a[1])
+
+    return frequencyArray.map(([item, frequency]) => ({ item, frequency }))
+}
