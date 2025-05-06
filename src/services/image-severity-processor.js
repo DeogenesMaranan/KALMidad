41a77b8781f6cc115777775079e68303b557098f
Deogenes Maranan <maranandeogenes@gmail.com>
2025-05-06 21:56:47 +0800

    Revert conversion of ImageSeverityProcessor

--- src/services/image-severity-processor.js
+++ src/services/image-severity-processor.js
@@ -1,28 +1,24 @@
-class ImageSeverityProcessor {
-    static async floodProcessor(p_image) {
-        const modelURL = '../../services/image-models/flood/model.json';
-        const metadataURL = '../../services/image-models/flood/metadata.json';
+// Retain not being a class as it causes error
 
-        const model = await tmImage.load(modelURL, metadataURL);
-        const prediction = await model.predict(p_image);
+async function floodProcessor(p_image) {
+    const modelURL = '../../services/image-models/flood/model.json'
+    const metadataURL = '../../services/image-models/flood/metadata.json'
 
-        return this.getLargest(prediction);
-    }
+    let model = await tmImage.load(modelURL, metadataURL)
+    const prediction = await model.predict(p_image)
 
-    static getLargest(array) {
-        let largest = 0;
-        let name = '';
-
-        array.forEach(element => {
-            if (element.probability > largest) {
-                largest = element.probability;
-                name = element.className;
-            }
-        });
-
-        return name;
-    }
+    return getLargest(prediction)
 }
 
-export const floodProcessor = ImageSeverityProcessor.floodProcessor;
-export const getLargest = ImageSeverityProcessor.getLargest;
+function getLargest(array) {
+    var largest = 0
+    var name = ''
+
+    array.forEach(element => {
+        if (element.probability > largest) {
+            largest = element.probability
+            name = element.className
+        }
+    });
+    return name
+}
